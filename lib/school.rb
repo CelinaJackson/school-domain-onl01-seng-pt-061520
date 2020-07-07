4db2e9ee3db1218ed75ff2817422a22eca4d78a2
Celina Jackson <celinanjackson@gmail.com>
2020-07-07 22:22:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -16,11 +16,3 @@ def add_student(name, grade)
   roster[grade] << name
 end
 end
-
-school = School.new("Bayside High School")
-school.roster
-school.add_student("AC Slater", 10)
-school.add_student("Jeff Baird", 10)
-school.add_student("Blake Johnson", 10)
-school.add_student("Homer Simpson", 9)
-school.add_student("Avi Flombaum", 10)
