efadd1d73ce8b420545b4e6edd7c98bab946af95
Celina Jackson <celinanjackson@gmail.com>
2020-07-07 22:18:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -32,3 +32,7 @@ end
 school = School.new("Bayside High School")
 school.roster
 school.add_student("AC Slater", 10)
+school.add_student("Jeff Baird", 10)
+school.add_student("Blake Johnson", 10)
+school.add_student("Homer Simpson", 9)
+school.add_student("Avi Flombaum", 10)
