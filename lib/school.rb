061dc5ec6242fae2edcc8b0ac306eed43903cdd5
Celina Jackson <celinanjackson@gmail.com>
2020-07-07 22:10:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,7 +5,12 @@ attr_accessor :name, :roster
   def initialize(name)
     @name = name
     @roster = {}
-  end 
+  end
+
+def add_student(student, level)
+  roster[level] ||= []
+  roster[level] << student
+end
 
 end
 
